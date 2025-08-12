efefab9bde320184f57bb8013f40711f6a30420e
Celestial <89109188+3rroronly1@users.noreply.github.com>
2025-08-12 17:59:00 +0530

    Update ccp3o1-scripts.js

--- assets/js/ccp3o1-scripts.js
+++ assets/js/ccp3o1-scripts.js
@@ -4,6 +4,7 @@ jQuery(document).ready(function($) {
         var visibleItems = parseInt($carousel.data('visible')) || 3;
         var speed = parseInt($carousel.data('speed')) || 300;
         var gap = parseInt($carousel.data('gap')) || 10;
+        var smooth = parseInt($carousel.data('smooth')) || 0;
 
         $carousel.css('--ccp3o1-gap', gap + 'px').slick({
             slidesToShow: visibleItems,
@@ -12,8 +13,10 @@ jQuery(document).ready(function($) {
             autoplaySpeed: speed,
             arrows: true,
             dots: true,
-            prevArrow: '<button type="button" class="slick-prev"><span class="dashicons dashicons-arrow-left-alt2"></span></button>',
-            nextArrow: '<button type="button" class="slick-next"><span class="dashicons dashicons-arrow-right-alt2"></span></button>'
+            speed: smooth ? Math.max(speed, 500) : speed,
+            cssEase: smooth ? 'ease-in-out' : 'ease',
+            prevArrow: '<button type="button" class="slick-prev" aria-label="Previous"><span class="dashicons dashicons-arrow-left-alt2"></span></button>',
+            nextArrow: '<button type="button" class="slick-next" aria-label="Next"><span class="dashicons dashicons-arrow-right-alt2"></span></button>'
         });
     });
 });
